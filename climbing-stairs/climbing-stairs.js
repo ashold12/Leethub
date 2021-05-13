ce991f925a4da204d53b62ff0290f7c58f87cc17
Alexander Shold <ashold12@gmail.com>
2021-05-13 14:08:06 -0500

    Time: 80 ms (37.92%), Space: 38.2 MB (79.73%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -4,7 +4,8 @@
  */
 
 var climbStairs = function(n) {
-    let memo = {0:0, 1:1, 2:2, 3:3}
+    //need base cases in memo enable to prevent infinite call stack
+    let memo = {0:0, 1:1, 2:2}
     let inner = (remaining) => {
         if (memo[remaining]) return memo[remaining]
         memo[remaining] = inner(remaining - 1) + inner(remaining - 2)
