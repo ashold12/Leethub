47d8905270ce1657a56cd2cef9c5200f7b956ed0
Alexander Shold <ashold12@gmail.com>
2021-05-27 01:32:55 -0500

    Time: 84 ms (63.86%), Space: 41 MB (5.68%) - LeetHub

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -12,15 +12,7 @@
 var reverseList = function(head) {
     let prev = null
     let curr = head
-    while (curr != null ) {
-        // next becomes previous (initialized as null at head)
-        // previous becomes current
-        // current becomes next
-        
-        // let nextStorage = curr.next
-        // curr.next = prev
-        // prev = curr
-        // curr = nextStorage
+    while (curr) {
         [curr.next, prev, curr] = [prev, curr, curr.next]
     }
     return prev
