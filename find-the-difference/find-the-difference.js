850f95f1d5e457da0858afdf8d7a973fc2d25fcb
Alexander Shold <ashold12@gmail.com>
2021-06-05 02:32:05 -0500

    Time: 80 ms (88.86%), Space: 39.2 MB (87.01%) - LeetHub

--- find-the-difference/find-the-difference.js
+++ find-the-difference/find-the-difference.js
@@ -4,25 +4,8 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let sDic = {}
-    let tDic = {}
+  let sum1 = s.split('').reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  let sum2 = t.split('').reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  return String.fromCharCode(sum2 - sum1);
     
-    for (let char of s) {
-        if (char in sDic) {
-            sDic[char]++
-            continue
-        }
-        sDic[char] = 1
-    }
-    for (let char of t) {
-        if (char in tDic) {
-            tDic[char]++
-        } else {
-            tDic[char] = 1
-        }
-        if (!sDic[char] || sDic[char] < tDic[char]) {
-            return char
-        }
-    }
-    return -1
 };
